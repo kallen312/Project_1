7f350851d96f8ff5a94d7d6d20a4305da0774250
kallen312 <73615486+kallen312@users.noreply.github.com>
2021-11-10 18:31:24 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+let first = ["Keturah", "Matthew", "Judy"]
+
+function addName(firstName){
+return firstName;
+}
+
+
+let name = first.map(addName)
+
+export default function App() {
+  return (
+    <div className="App">
+      <table>
+        <th>First Name</th>
+        <tr>
+          <td>{name[0]}</td>
+        </tr>
+        <tr>
+          <td>{name[1]}</td>
+        </tr>
+        <tr>
+          <td></td>
+        </tr>
+        <tr>
+          <td>{name[2]}</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
